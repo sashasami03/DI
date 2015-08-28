0fedcd26fba21b20e94c69a6c54d5f9f8f434cf5
sasha <sashasami03@gmail.com>
2015-08-29 04:53:55 +0530

    updated inequalities

--- No_Signalling/inequalities.py
+++ No_Signalling/inequalities.py
@@ -3,21 +3,16 @@
 ##limit : 0 - 8 ( divide by 8.0 to probability )
 
 def mermin_game_1():
-  mermin_p = []
+  mermin = []
   for x in range(2):
     for y in range(2):
       for z in range(2):
-        if x == 1 and y == 1 and z == 1:
-          continue
-        mermin_p.append([[0,1,2],[x,y,z],[0,0,0],1])
-        mermin_p.append([[0,1,2],[x,y,z],[1,1,0],1])
-        mermin_p.append([[0,1,2],[x,y,z],[1,0,1],1])
-        mermin_p.append([[0,1,2],[x,y,z],[0,1,1],1])
-  mermin_p.append([[0,1,2],[1,1,1],[1,0,0],1])
-  mermin_p.append([[0,1,2],[1,1,1],[0,1,0],1])
-  mermin_p.append([[0,1,2],[1,1,1],[0,0,1],1])
-  mermin_p.append([[0,1,2],[1,1,1],[1,1,1],1])
-  return mermin_p
+        for a in range(2):
+	  for b in range(2):
+	    for c in range(2):
+	      if ( a ^ b ^ c ) == ( x * y * z):
+	        mermin.append([[0,1,2],[x,y,z],[a,b,c],1])  	 
+  return mermin
 
 
 ##Mermin game a xor b xor c = x.y xor y.z	
@@ -27,40 +22,27 @@ def mermin_game_2():
   for x in range(2):
     for y in range(2):
       for z in range(2):
-        if (x * y) ^ (z * x)  ^ (y * z)   == 0 :
-          mermin.append([[0,1,2],[x,y,z],[0,0,0],1])
-          mermin.append([[0,1,2],[x,y,z],[1,1,0],1])
-          mermin.append([[0,1,2],[x,y,z],[1,0,1],1])
-          mermin.append([[0,1,2],[x,y,z],[0,1,1],1])
-        else :
-          mermin.append([[0,1,2],[x,y,z],[1,0,0],1])
-          mermin.append([[0,1,2],[x,y,z],[0,1,0],1])
-          mermin.append([[0,1,2],[x,y,z],[0,0,1],1])
-          mermin.append([[0,1,2],[x,y,z],[1,1,1],1])
-   
-  return mermin 
-
+        for a in range(2):
+	  for b in range(2):
+	    for c in range(2):
+	      if ( a ^ b ^ c ) == ( (x * y) ^ (y * z)  ):
+	        mermin.append([[0,1,2],[x,y,z],[a,b,c],1])  	 
+  return mermin
 
 
 ##Svetlichny game a xor b xor c = x.y xor y.z xor z.x
 ##limit : 0 - 8 ( divide by 8.0 to get probability )	
 def svetlichny_game():
-  svet = []
+  stev = []
   for x in range(2):
     for y in range(2):
       for z in range(2):
-        if (x * y) ^ (z * x)  ^ (y * z)   == 0 :
-          svet.append([[0,1,2],[x,y,z],[0,0,0],1])
-          svet.append([[0,1,2],[x,y,z],[1,1,0],1])
-          svet.append([[0,1,2],[x,y,z],[1,0,1],1])
-          svet.append([[0,1,2],[x,y,z],[0,1,1],1])
-        else :
-          svet.append([[0,1,2],[x,y,z],[1,0,0],1])
-          svet.append([[0,1,2],[x,y,z],[0,1,0],1])
-          svet.append([[0,1,2],[x,y,z],[0,0,1],1])
-          svet.append([[0,1,2],[x,y,z],[1,1,1],1])
-   
-  return svet
+        for a in range(2):
+	  for b in range(2):
+	    for c in range(2):
+	      if ( a ^ b ^ c ) == ( (x * y) ^ (y * z) ^ (z * x)  ):
+	        stev.append([[0,1,2],[x,y,z],[a,b,c],1])  	 
+  return stev
 
 ##Mermin fact <A_1B_0C_0> + <A_0B_1C_0> + <A_0B_0C_1> - <A_1B_1C_1>	
 ##limit : -4 - 4 
